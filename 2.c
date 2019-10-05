426b2d54478b60afd457e1f676f341d65c1bd073
uday kumar <udaykumara0478@gmail.com>
2019-10-05 13:04:46 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct
+void create(){
+    FILE *fp;
+    fp = fopen("stu_db.bin","rb+");
+        if(fp == NULL){
+            fp = fopen("stu_db.bin","wb+");
+            if(fp == NULL){
+                printf("Connot open file");
+                exit(1)
+}
+void main()
+{
+    label:printf("\t\tM A I N  M E N U\n\t1. Create the Binary File\n\t2. Display the Contents of the file\n\t3. Seek a specific record\n\t4. Update the contents of a record\n\t5. Delete a record fro the specific name\n\t6. Exit\n\n\t\tPlease Enter your choice...[]");
+    int choice;
+    switch (choice)
+    {
+    case 1:
+        create();
+        break;
+    case 2:
+        disp();
+        break;
+    case 3:
+        seek();
+        break;
+    case 4:
+        update();
+        break;
+    case 5:
+        delete();
+        break;
+    case 6:
+        exit(1);
+    default:
+    printf("incorrect choice\n");
+    goto label;
+        break;
+    }
+
+}
